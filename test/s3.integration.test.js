693c2e97fd099229b81aba7bd293a1f031bccd10
cavis <ryancavis@gmail.com>
2025-12-15 13:39:48 -0700

    Use putProgress in testing

--- test/s3.integration.test.js
+++ test/s3.integration.test.js
@@ -4,7 +4,7 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
-import { getProgress, path } from "../lib/s3";
+import { getProgress, path, putProgress } from "../lib/s3";
 
 const client = new S3Client();
 
@@ -21,20 +21,19 @@ describe("s3 integration", () => {
     }
   });
 
-  it("gets the latest wip recording", async () => {
+  it.skip("gets the latest wip recording", async () => {
     expect(await getProgress(rec)).toBeNull();
 
     const b1 = "something non json";
     const b2 = '{"thing":"two"}';
-    const b3 = '{"thing":"three"}';
-
     await client.send(new PutObjectCommand({ Bucket, Body: b1, Key: `${Prefix}/one.flac` }));
     await client.send(new PutObjectCommand({ Bucket, Body: b2, Key: `${Prefix}/two.mp3.wip` }));
     expect(await getProgress(rec)).toEqual({ thing: "two" });
 
     // send something with LastModified 1 second later - it wins
     await new Promise((r) => setTimeout(r, 1000));
-    await client.send(new PutObjectCommand({ Bucket, Body: b3, Key: `${Prefix}/three.aac.wip` }));
+    rec.filename = "three.aac";
+    await putProgress(rec, { thing: "three" });
     expect(await getProgress(rec)).toEqual({ thing: "three" });
   });
 });
