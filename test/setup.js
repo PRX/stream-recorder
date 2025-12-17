782009db10b92ba7b487e2f89b5273b41e0926ba
cavis <ryancavis@gmail.com>
2025-12-17 11:39:11 -0700

    Consolidate S3 mocking

--- test/setup.js
+++ test/setup.js
@@ -3,6 +3,8 @@ beforeEach(() => {
   process.env.BUFFER_END = "";
   process.env.CONFIG_URL = "http://my.config/streams.json";
   process.env.OXBOW_SNS_TOPICS = "topic1,topic2";
+  process.env.OXBOW_STARTUP_TIME = "120";
+  process.env.OXBOW_WIP_TIME = "30";
   process.env.S3_BUCKET = "my-bucket";
   process.env.S3_PREFIX = "";
 });
---
+++ test/support.js
@@ -0,0 +1,52 @@
+import "aws-sdk-client-mock-jest";
+import { Readable } from "node:stream";
+import {
+  GetObjectCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { sdkStreamMixin } from "@smithy/util-stream";
+import { mockClient } from "aws-sdk-client-mock";
+
+const s3Mock = mockClient(S3Client);
+const makeStream = (str) => {
+  const stream = new Readable();
+  stream.push(str);
+  stream.push(null);
+  return sdkStreamMixin(stream);
+};
+
+/**
+ * Mock list requests
+ */
+export function mockS3List(val) {
+  const listObjects = async (params) => {
+    return typeof val === "function" ? val(params) : val;
+  };
+  s3Mock.on(ListObjectsCommand).callsFake(listObjects);
+}
+
+/**
+ * Mock get requests, handling the messy Body streaming bit
+ */
+export function mockS3Get(val) {
+  const getObject = async (params) => {
+    const res = typeof val === "function" ? val(params) : val;
+    if (res.Body) {
+      res.Body = makeStream(res.Body);
+    }
+    return res;
+  };
+  s3Mock.on(GetObjectCommand).callsFake(getObject);
+}
+
+/**
+ * Mock put requests
+ */
+export function mockS3Put(val) {
+  const putObject = async (params) => {
+    return typeof val === "function" ? val(params) : val;
+  };
+  s3Mock.on(PutObjectCommand).callsFake(putObject);
+}
